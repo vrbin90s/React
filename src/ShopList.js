1a0f2a11384009c2b843ea361d2dd9bc869df7ff
agree <ks3319t@gre.ac.uk>
2023-06-01 10:01:21 +0300

    shop list component

---
+++ src/ShopList.js
@@ -0,0 +1,49 @@
+import ShopItem from "./ShopItem";
+
+function ShopList() {
+
+    const shops = [
+        {
+            title: "Parduotuve 1",
+            phone: "+37045431321",
+            email: "info@parduotuve.lt",
+            address: "Vilniaus g. 20, Vilnius"
+        },
+        {
+            title: "Parduotuve 2",
+            phone: "+37045412247",
+            email: "info@parduotuve.lt",
+            address: "Kęstučio g. 20, Viduklė"
+        },
+        {
+            title: "Parduotuve 3",
+            phone: "+3704445568",
+            email: "info@parduotuve.lt",
+            address: "Vilniaus g. 20, Klaipėda"
+        },
+        {
+            title: "Parduotuve 4",
+            phone: "+3704545689",
+            email: "info@parduotuve.lt",
+            address: "Karaliaus g. 20, Kaunas"
+        }
+        ];
+
+        return (
+            <div className="shops-list">
+                {shops.map((shop, index) => {
+                    return (
+                    <ShopItem
+                    key={index}
+                    title={shop.title}
+                    phone={shop.phone}
+                    email={shop.email}
+                    address={shop.address}/>
+                    )
+                })}
+            </div>
+        )
+    
+}
+
+export default ShopList;
